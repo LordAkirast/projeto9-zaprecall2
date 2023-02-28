3fddb445f8c07fec206ff3002dde0aaacb46b7e5
LordAkirast <darkwarish@gmail.com>
2023-02-28 03:03:00 -0300

    Tela inicial criada. Botão extras criado. Easter Egg raio adicionado. Animações e fontes adicionadas.

---
+++ src/assets/components/Welcome.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import '../style/index.css';
+import logo from '../images/logo.png';
+
+export default function Welcome() {
+  const [start, setstart] = useState(0);
+
+  function runBarryRun() {
+    window.location.href = 'https://www.youtube.com/watch?v=8D4ThTcvTDo';
+  }
+
+  return (
+    <div className='container'>
+      <img 
+        className='logo'
+        src={logo}
+        alt='logo'
+        style={{ width: 136, height: 161 }}
+        onClick={runBarryRun}
+      />
+      <p style={{ fontFamily: 'Righteous, sans-serif', fontSize: '36px', color: 'white' }}>ZapRecall</p>
+
+      <button  className='initiate' onClick={begin}>
+        Iniciar Recall
+      </button>
+      <button disabled={true} className='extra' onClick={begin}>
+        Extras Mode
+      </button>
+    </div>
+  );
+
+
+  /// a ideia aqui é que quando for 1 então deve mudar de tela. o valor deve mudar ao clicar no initiate
+  function begin() {
+    setstart(1);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/style/index.css'
+import Welcome from './assets/components/Welcome.js';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Welcome />
 
 );
 
 
+///o que fazer?
+/// criar um novo componente onde ao o valor de initiate for 1 ele muda.
+// tem que aprender a usar ternário para renderizar. ou if else.
